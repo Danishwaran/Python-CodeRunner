17c9d1100468e3619d95219ac3898b0ff4737a69
Danishwaran <ds2403@srmist.edu.in>
2022-06-30 00:43:23 +0530

    Load local script and Save script.

--- public/script.js
+++ public/script.js
@@ -3,11 +3,47 @@ const editor = document.getElementById("editor");
 const runBtn = document.getElementById("submit");
 const output = document.getElementById("output");
 const reset = document.getElementById("reset");
+const save = document.getElementById("save");
+const load = document.getElementById("load");
 
 //reset editor and console on click
 reset.addEventListener("click", function() {
   editor.value = "";
   output.value = "";
+})
+
+//save code 
+save.addEventListener("click", function() {
+
+  const content = editor.value;
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+  element.setAttribute('download', "pythonfile.py");
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+})
+
+//load code
+load.addEventListener("change", function() {
+  var file = load.files[0];
+  var reader = new FileReader();
+
+  var pythonFile = /python.*/;
+
+  if (file.type.match(pythonFile)) {
+    reader.onload = function (event) {
+      editor.value = event.target.result;
+    }
+  }
+  else {
+    editor.value = "It doesn't seem to be a python file!";
+  }
+  reader.readAsText(file);
 })
 
 //initiate code compile on click
@@ -52,7 +88,7 @@ function displayOutput(result) {
     else {
       output.value = "";
     }
-    console.log(output);
+
   }
   else {
     output.value = result.error;
